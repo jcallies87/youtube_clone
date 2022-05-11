72c42de302e30e3c8a161a46d2775d95102bf985
Justin Callies <jcallies@gmail.com>
2022-05-11 15:06:08 -0500

    created youtube_reply app. removed reply model from youtube_clone and put it into youtube_reply app

--- backend/drf_jwt_backend/settings.py
+++ backend/drf_jwt_backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'cars.apps.CarsConfig',  # This app is for example use only
     'youtube_clone',
+    'youtube_reply'
 ]
 
 MIDDLEWARE = [
--- backend/youtube_clone/models.py
+++ backend/youtube_clone/models.py
@@ -9,8 +9,3 @@ class Comment(models.Model):
     text = models.CharField(max_length=255)
     likes = models.IntegerField()
     dislikes = models.IntegerField()
-
-class Reply(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
-    text = models.CharField(max_length=255)
