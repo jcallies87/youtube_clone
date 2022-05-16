5f08e6f86cdfb569d4b2a8576b459b662c283cfb
Justin Callies <jcallies@gmail.com>
2022-05-16 16:21:55 -0500

    created searchbar.jsx finished writting code for homepage.js

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -10,13 +10,15 @@ const HomePage = () => {
   const [user, token] = useAuth();
   // const [cars, setCars] = useState([]);
   const [searchResults, setSearchResults] = useState([])
-  const [videoId, setVideoId] = useState("BTPcq2HczVY")
-  const [searchTerm, setSearchTerm] = useState("cats")
+  const [videoId, setVideoId] = useState("mqqft2x_Aa4")
+  const [searchTerm, setSearchTerm] = useState("dogs")
+
 
   const getSearchResults = async () => {
     try {
       let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${searchTerm}&key=AIzaSyAGmeDzCGv9RmU7S5NxRSFCjiwbmys9ltQ&part=snippet`)
       setSearchResults(response.data.items)
+
     }catch(ex){console.log (ex)}
   }
 
@@ -42,39 +44,47 @@ const HomePage = () => {
 
   return (
     <div>
+
       <div className= "suggestedVideos" > 
         
         {searchResults[0]&&<h3 >{searchResults[0].snippet.title}</h3>}
-        {searchResults[0]&&<img src= {searchResults[0].snippet.thumbnails.high.url}/>}
+        {searchResults[0]&&<a href= {searchResults[0].id.videoId} ><img src= {searchResults[0].snippet.thumbnails.high.url}alt =""/></a>}
         {searchResults[0]&&<p>{searchResults[0].snippet.description}</p>}
+        
         {searchResults[1]&&<h3 >{searchResults[1].snippet.title}</h3>}
-        {searchResults[1]&&<img src= {searchResults[1].snippet.thumbnails.high.url}/>}
+        {searchResults[1]&&<a href= {searchResults[1].id.videoId} ><img src= {searchResults[1].snippet.thumbnails.high.url}alt =""/></a>}
         {searchResults[1]&&<p>{searchResults[1].snippet.description}</p>}
+        
         {searchResults[2]&&<h3 >{searchResults[2].snippet.title}</h3>}
-        {searchResults[2]&&<img src= {searchResults[2].snippet.thumbnails.high.url}/>}
+        {searchResults[2]&&<a href= {searchResults[2].id.videoId} ><img src= {searchResults[2].snippet.thumbnails.high.url}alt =""/></a>}
         {searchResults[2]&&<p>{searchResults[2].snippet.description}</p>}
+        
         {searchResults[3]&&<h3 >{searchResults[3].snippet.title}</h3>}
-        {searchResults[3]&&<img src= {searchResults[3].snippet.thumbnails.high.url}/>}
+        {searchResults[3]&&<a href= {searchResults[3].id.videoId} ><img src= {searchResults[3].snippet.thumbnails.high.url}alt =""/></a>}
         {searchResults[3]&&<p>{searchResults[3].snippet.description}</p>}
+        
         {searchResults[4]&&<h3 >{searchResults[4].snippet.title}</h3>}
-        {searchResults[4]&&<img src= {searchResults[4].snippet.thumbnails.high.url}/>}
+        {searchResults[4]&&<a href= {searchResults[4].id.videoId} ><img src= {searchResults[4].snippet.thumbnails.high.url}alt =""/></a>}
         {searchResults[4]&&<p>{searchResults[4].snippet.description}</p>}
       </div>
     <div className="container">
       <h1>Home Page for {user.username}!</h1>
-      <button onClick={() => getSearchResults()}>click for vids</button>
-
       <iframe id="ytplayer" type="text/html" width="640" height="360"
-  src= {`https://www.youtube.com/embed/${videoId}`}
-  frameBorder="0"></iframe>
-  
+        src= {`https://www.youtube.com/embed/${videoId}`}
+        frameBorder="0">
+      </iframe>
+      </div>
+    <div className="container">
+      <h3>comments</h3>
+    </div>
+      
       {/* {cars &&
         cars.map((car) => (
           <p key={car.id}>
             {car.year} {car.make} {car.model}
           </p>
         ))} */}
-    </div></div>
+    </div>
 
   );
 };
